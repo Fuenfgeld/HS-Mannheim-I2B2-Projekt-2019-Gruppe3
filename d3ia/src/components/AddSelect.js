078bce84281a8978a9d9451d8b9df1e87644b273
Nabe Al Hasnawi <nabe.alhasnawi@gmail.com>
2019-05-31 11:53:17 +0200

    react bootstrap components

--- d3ia/src/components/AddSelect.js
+++ d3ia/src/components/AddSelect.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Button} from 'reactstrap'
+
 
 export class AddSelect extends Component {
-  state = {
+ state = {
     title: ''
   }
 
-  ADD = (e) => {
+  onSubmit = (e) => {
     e.preventDefault();
     this.props.addSelect(this.state.title);
     this.setState({ title: '' });
@@ -14,31 +16,24 @@ export class AddSelect extends Component {
 
   onChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
+
   render() {
     return (
-      <form onSubmit={this.ADD} style={{ display: 'flex' }}>
-        <input
-          type="text"
-          name="title"
-          style={{ flex: '10', padding: '5px' }}
-          placeholder="Add"
-          value={this.state.title}
-          onChange={this.onChange}
-        />
-        <input
-          type="ADD"
-          value="ADD"
-          className="btn"
-          style={{flex: '1'}}
-        />
-      </form>
+       <Button
+           onSubmit={this.onSubmit}
+           style={{ display: 'flex' }}
+           onChange={this.onChange}
+           >
+           ADD
+       </Button>
     )
   }
 }
 
+
 // PropTypes
 AddSelect.propTypes = {
-  addTodo: PropTypes.func.isRequired
+  addSelect: PropTypes.func.isRequired
 }
 
 export default AddSelect
