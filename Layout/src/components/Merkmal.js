6cf011c02cf16fd5ef07da8ebf02aa3672052a3e
AnetteKoch <1723467@stud.hs-mannheim.de>
2019-06-04 10:56:35 +0200

    Merkmal  Dropdown

--- Layout/src/components/Merkmal.js
+++ Layout/src/components/Merkmal.js
@@ -4,21 +4,22 @@ import React, {Component} from "react";
 class Merkmal extends Component{
     constructor(props) {
         super(props);
-        this.state = {
-
-        return(
-            <div>Name
-                <div id="Dropdown">
-                    <button  id = "dropbtn">AND</button>
-                    <div id ="Dropdown-content">
-                    <a href = "#">AND</a>
-                    <a href = "#">OR</a>
+        this.state = {}}
+            render() {
+                return (
+                    <div id = "Merkmal">
+                        <div class="dropdown">
+                            <button class="dropbtn">AND</button>
+                            <div class="dropdown-content">
+                                <a href="#">AND</a>
+                                <a href="#">OR</a>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-        )
+                )
+            }
 
-}
+        }
 
 
 
--- Layout/src/components/Selektion.js
+++ Layout/src/components/Selektion.js
@@ -5,29 +5,30 @@ import Merkmal from "./Merkmal";
 function Selection(){
  var list = [ ];
 
- function handleClick(e) {
-     e.preventDefault();
-
-     list.push('<a> <Merkmal href="#" >Merkmal</Merkmal> </a>');
-
-     var i;
-     for (i = 0; i < list.length; i++) {
-         var selectionList = list[i];
-         var ausgabe = document.getElementById('ausgabe');
-         ausgabe.innerHTML = selectionList;
-     }
-
+ function handleClick() {
+     var mark =  '<a><Merkmal></Merkmal></a>'
+     list.push(mark);
+     print();
  }
 
-function init() {
-     var elem = document.getElementById('Add');
-     elem.addEventListener('click', handleClick);
-}
+ function print() {
+     var i = 0;
+     var selectionLst = list[i];
+     var ausgeben = document.getElementById("ausgabe");
+     ausgeben.innerHTML += list[i]
+     i = i+1;
+ }
+
 
  return(
 
      <div class = "scrollMenu" >
-         <script>document.addEventListener('DOMContentLoaded', init);</script>
+         <div id="ausgabe"><script>
+         var elem = document.getElmentById(Add);
+         if(elem.addEventListenen){
+             document.addEventListener("click", handleClick, false)
+             }</script></div>
+
      </div>
      );
 
