8860e0b5dde726de3ec8179f055aad1025faa22b
Leo <1726586@stud.hs-mannheim.de>
2019-06-19 13:21:42 +0200

    V3.3
    -current version

---
+++ front-alpha-v3.0/src/components/Layout/Edit.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+
+class Edit extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return(
+                <button onClick= "myFunction()">Edit</button>
+        )
+    }
+}
+
+export default Edit;
